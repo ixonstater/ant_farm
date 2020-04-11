0f78cb5e9ce3efaf71e67ad927672f09b18697e1
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-04-11 12:32:58 -0600

    Removed JSON editor, no one was really going to use it anyway

--- lib/pages/new_map.dart
+++ lib/pages/new_map.dart
@@ -9,19 +9,15 @@ class NewMap extends StatefulWidget{
 }
 
 class _NewMapState extends State<NewMap>{
-  static const int jsonEditor = 0;
-  static const int guiMapCreator = 1;
-  static const int mapUpload = 2;
+  static const int guiMapCreator = 0;
+  static const int mapUpload = 1;
   var key;
   _NewMapState(this.key);
-  int displayedContent = _NewMapState.jsonEditor;
+  int displayedContent = _NewMapState.guiMapCreator;
   String currentMap = "{}";
 
   selectBody(){
-    if(this.displayedContent == _NewMapState.jsonEditor){
-      return JSONEditor(key, currentMap);
-    }
-    else if (this.displayedContent == _NewMapState.guiMapCreator){
+    if (this.displayedContent == _NewMapState.guiMapCreator){
       return GUIMapMaker(key, currentMap);
     }
     else if (this.displayedContent == _NewMapState.mapUpload){
@@ -40,16 +36,6 @@ class _NewMapState extends State<NewMap>{
       endDrawer: Drawer(
         child: ListView(
           children: <Widget>[
-            ListTile(
-              onTap: () => setState(() {
-                this.displayedContent = _NewMapState.jsonEditor;
-                Navigator.pop(build);
-              }),
-              title: Text(
-                "JSON Editor",
-                style: AppThemes.listTileText(),
-              ),
-            ),
             ListTile(
               onTap: () => setState(() {
                 this.displayedContent = _NewMapState.guiMapCreator;
@@ -76,133 +62,6 @@ class _NewMapState extends State<NewMap>{
       body: this.selectBody(),
       resizeToAvoidBottomPadding: true,
     );
-  }
-}
-
-class JSONEditor extends StatelessWidget{
-  final String map;
-  JSONEditor(Key key, this.map) : super(key: key);
-
-  Widget build(BuildContext build){
-    var mediaData = MediaQuery.of(build);
-    var height = mediaData.size.height;
-    var width = mediaData.size.width;
-    
-    if(height > width){
-      return ListView(
-        children: <Widget>[
-          SizedBox(height: 10),
-          Text("Enter JSON Here"),
-          SizedBox(height: 5),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
-                width: 2
-              )
-            ),
-            margin: EdgeInsets.only(
-              left: 10,
-              right: 10
-            ),
-            child: TextField(
-              maxLines: 15,
-              controller: TextEditingController.fromValue(
-                TextEditingValue(
-                  text: this.map
-                )
-              )
-            ),
-          ),
-          SizedBox(height: 10),
-          Align(
-            alignment: Alignment.center,
-            child: RaisedButton(
-              onPressed: (){},
-              child: Text(
-                "Validate",
-                style: AppThemes.buttonText()
-              )
-            ),
-          ),
-          SizedBox(height: 10),
-          Align(
-            alignment: Alignment.center,
-            child: RaisedButton(
-              onPressed: (){},
-              child: Text(
-                "Render",
-                style: AppThemes.buttonText()
-              )
-            ),
-          ),
-        ],
-      );
-    }
-    else {
-      return Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2
-                )
-              ),
-              child: ListView(
-                children: <Widget> [
-                  SizedBox(height: 10),
-                  Text(
-                    "Enter JSON Here"
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    maxLines: 999999,
-                    scrollPadding: EdgeInsets.all(20.0),
-                    controller: TextEditingController.fromValue(
-                      TextEditingValue(
-                        text: this.map
-                      )
-                    )
-                  ),
-                ]
-              ),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-             crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(width: 10),
-                Align(
-                  alignment: Alignment.center,
-                  child: RaisedButton(
-                    onPressed: (){},
-                    child: Text(
-                      "Validate",
-                      style: AppThemes.buttonText()
-                    )
-                  ),
-                ),
-                SizedBox(width: 10),
-                Align(
-                  alignment: Alignment.center,
-                  child: RaisedButton(
-                    onPressed: (){},
-                    child: Text(
-                      "Render",
-                      style: AppThemes.buttonText()
-                    )
-                  ),
-                )
-              ]
-            )
-          )
-        ]
-      );
-    }
   }
 }
 
